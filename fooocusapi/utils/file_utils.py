ccc770ab7920e7d1b77e73dd0ace07585d88fbaa
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-04-22 00:25:41 +0530

    Update file_utils.py
    added s3 bucket support

--- fooocusapi/utils/file_utils.py
+++ fooocusapi/utils/file_utils.py
@@ -1,38 +1,22 @@
-# -*- coding: utf-8 -*-
-
-""" File utils
-
-Use for managing generated files
-
-@file: file_utils.py
-@author: Konie
-@update: 2024-03-22
-"""
 import base64
 import datetime
 from io import BytesIO
 import os
-import json
-from pathlib import Path
 import numpy as np
 from PIL import Image
+import uuid
+import json
+from pathlib import Path
 from PIL.PngImagePlugin import PngInfo
-
-from fooocusapi.utils.logger import logger
+import boto3
 
 
 output_dir = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../..', 'outputs', 'files'))
+    os.path.dirname(__file__), '..', 'outputs', 'files'))
 os.makedirs(output_dir, exist_ok=True)
 
-STATIC_SERVER_BASE = 'http://127.0.0.1:8888/files/'
-
-
-def save_output_file(
-        img: np.ndarray,
-        image_meta: dict = None,
-        image_name: str = '',
-        extension: str = 'png') -> str:
+def save_output_file(img: np.ndarray, image_meta: dict = None,
+                     image_name: str = '', extension: str = 'png') -> str:
     """
     Save np image to file
     Args:
@@ -46,6 +30,8 @@ def save_output_file(
     current_time = datetime.datetime.now()
     date_string = current_time.strftime("%Y-%m-%d")
 
+    image_name = str(uuid.uuid4()) if image_name == '' else image_name
+
     filename = os.path.join(date_string, image_name + '.' + extension)
     file_path = os.path.join(output_dir, filename)
 
@@ -58,8 +44,7 @@ def save_output_file(
     meta = None
     if extension == 'png':
         meta = PngInfo()
-        meta.add_text("parameters", json.dumps(image_meta))
-        meta.add_text("fooocus_scheme", image_meta['metadata_scheme'])
+        meta.add_text("params", json.dumps(image_meta))
 
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     Image.fromarray(img).save(file_path, format=extension,
@@ -68,28 +53,16 @@ def save_output_file(
 
 
 def delete_output_file(filename: str):
-    """
-    Delete files specified in the output directory
-    Args:
-        filename: str of file name
-    """
     file_path = os.path.join(output_dir, filename)
     if not os.path.exists(file_path) or not os.path.isfile(file_path):
-        logger.std_warn(f'[Fooocus API] {filename} not exists or is not a file')
+        return
     try:
         os.remove(file_path)
-        logger.std_info(f'[Fooocus API] Delete output file: {filename}')
     except OSError:
-        logger.std_error(f'[Fooocus API] Delete output file failed: {filename}')
+        print(f"Delete output file failed: {filename}")
 
 
 def output_file_to_base64img(filename: str | None) -> str | None:
-    """
-    Convert an image file to a base64 string.
-    Args:
-        filename: str of file name
-    return: str of base64 string
-    """
     if filename is None:
         return None
     file_path = os.path.join(output_dir, filename)
@@ -100,17 +73,11 @@ def output_file_to_base64img(filename: str | None) -> str | None:
     output_buffer = BytesIO()
     img.save(output_buffer, format='PNG')
     byte_data = output_buffer.getvalue()
-    base64_str = base64.b64encode(byte_data).decode('utf-8')
+    base64_str = base64.b64encode(byte_data)
     return base64_str
 
 
 def output_file_to_bytesimg(filename: str | None) -> bytes | None:
-    """
-    Convert an image file to a bytes string.
-    Args:
-        filename: str of file name
-    return: bytes of image data
-    """
     if filename is None:
         return None
     file_path = os.path.join(output_dir, filename)
@@ -125,12 +92,15 @@ def output_file_to_bytesimg(filename: str | None) -> bytes | None:
 
 
 def get_file_serve_url(filename: str | None) -> str | None:
-    """
-    Get the static serve url of an image file.
-    Args:
-        filename: str of file name
-    return: str of static serve url
-    """
     if filename is None:
         return None
-    return STATIC_SERVER_BASE + filename.replace('\\', '/')
+    s3_client = boto3.client('s3', aws_access_key_id="", aws_secret_access_key="")
+    BUCKET_NAME = ''
+    FOLDER_NAME = ''
+    file_path = os.path.join(output_dir, filename)
+    image_name = f"generated_image_{os.urandom(4).hex()}.webp"
+    s3_client.upload_file(file_path , BUCKET_NAME,FOLDER_NAME+image_name)
+    static_serve_base_url = 's3-bucket-url'
+    print(static_serve_base_url)
+    print(file_path)
+    return static_serve_base_url + image_name
